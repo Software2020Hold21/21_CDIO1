59b7bc2b1c732af4a0f3db41d66478f832e064e2
Marie Seindal <marie@seindal.dk>
2020-02-26 11:14:52 +0100

    Oprettet tui-branch og arbejdet på tui

--- src/view/TUI.java
+++ src/view/TUI.java
@@ -1,6 +1,7 @@
 package view;
 
 import dal.IUserDAO;
+import java.util.Scanner;
 
 public class TUI {
     private IUserDAO userDAO;
@@ -10,6 +11,38 @@ public class TUI {
     }
 
     public void mainMenu(){
-        System.out.println("BEGYND");
+        System.out.println("Menu");
+
+        //Display options
+        System.out.println("1: Create new user.");
+        System.out.println("2: View existing users.");
+        System.out.println("3: Edit existing account");
+        System.out.println("4: Delete account");
+        System.out.println("5: Exit program");
+
+
+        Scanner input = new Scanner(System.in);
+        int choice = input.nextInt();
+
+        switch (choice){
+            case 1:
+                // Create. call method for this menu option
+                break;
+            case 2:
+                // view.
+                break;
+            case 3:
+                // edit
+                break;
+            case 4:
+                // delete
+                break;
+            case 5:
+
+
+        }
+
+
+
     }
 }
